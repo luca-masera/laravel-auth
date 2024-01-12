3be41ff21d1102aceed61bfdd95d7aca7967c6a5
luca-masera <lucamasera1995@gmail.com>
2024-01-12 18:15:33 +0100

    table index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,8 +2,23 @@
 @section('content')
     <section class="container">
         <h1>Lista Posts</h1>
-        @foreach ($projects as $project)
-            <p><a href="{{ route('admin.projects.show', $project->id) }}">{{ $project->title }}</a></p>
-        @endforeach
+        <table class="table">
+            <tbody>
+                @foreach ($projects as $project)
+                    <tr>
+                        <td>{{ $project->title }}</td>
+                        <td class="d-flex justify-content-end align-items-center">
+                            <a href="{{ route('admin.projects.show', $project->id) }}" class="btn btn-success ">Mostra</a>
+
+                            <form action="{{ route('admin.projects.destroy', $project->id) }}">
+                                <button type="submit">Elimina</button>
+                            </form>
+
+                        </td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
     </section>
 @endsection
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,5 +3,6 @@
     <section class="container">
         <h1>{{ $project->title }}</h1>
         <p>{{ $project->body }}</p>
+        <a href="{{ route('admin.projects.edit', $project->id) }}" class="btn btn-success ">Modifica</a>
     </section>
 @endsection
