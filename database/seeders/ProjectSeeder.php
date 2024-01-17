4f5419b725415e670fff59e5f160760999fb7bb3
luca-masera <lucamasera1995@gmail.com>
2024-01-17 13:24:03 +0100

    nome alla colonna image nel db

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Project;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class ProjectSeeder extends Seeder
 {
@@ -18,7 +19,7 @@ class ProjectSeeder extends Seeder
 
         foreach ($projects as $project) {
             $newProject = new Project();
-            $newProject->image = $project['image'];
+            $newProject->image = ProjectSeeder::storeimage($project['image'], $project['title']);
             $newProject->title = $project['title'];
             $newProject->body = $project['body'];
             $newProject->user_id = 1;
@@ -27,4 +28,16 @@ class ProjectSeeder extends Seeder
         }
 
     }
+
+    public static function storeimage($img, $name)
+    {
+        $url = $img;
+        $contents = file_get_contents($url);
+        $name = Str::slug($name, '-') . 'jpg';
+        $path = 'images/' . $name;
+        Storage::put('images/' . $name, $contents);
+        return $path;
+
+    }
+
 }
